b28a04730c6f38cbce12d709f6665d26d3ebb7a6
Lajos Nyeki <lajos_nyeki@epam.com>
2015-12-04 12:58:03 +0100

    UI a persistency added for hardware controller settings.

--- androidClient/HomeMoodLigtingClient/app/src/main/java/com/nyekilajos/homemoodlightingclient/persitency/LightingPreferencesHelper.java
+++ androidClient/HomeMoodLigtingClient/app/src/main/java/com/nyekilajos/homemoodlightingclient/persitency/LightingPreferencesHelper.java
@@ -12,22 +12,80 @@ import com.google.inject.Inject;
  */
 public class LightingPreferencesHelper {
 
-    private static final String PREFERENCES_NAME = "lightingPreferences";
+    private static final String PROGRAM_PREFERENCES_NAME = "programPreferences";
     private static final String ACTIVE_BEDROOM_PROGRAM = "activeBedroomProgram";
+    private static final String ACTIVE_LIVINGROOM_PROGRAM = "activeLivingroomProgram";
 
-    private final SharedPreferences sharedPreferences;
+    private static final String CONTROLLER_SETUP_PREFERENCES = "controllerSetupPreferences";
+    private static final String BEDROOM_IP_ADDRESS = "bedroomIpAddress";
+    private static final String BEDROOM_PORT = "bedroomPort";
+    private static final String LIVINGROOM_IP_ADDRESS = "livingroomIpAddress";
+    private static final String LIVINGROOM_PORT = "port";
+    private static final String LIVINGROOM_LED_COUNT = "livingroomLedCount";
+
+    private final SharedPreferences programPreferences;
+    private final SharedPreferences controllerPreferences;
 
     @Inject
     public LightingPreferencesHelper(Context context) {
-        sharedPreferences = context.getSharedPreferences(PREFERENCES_NAME, Context.MODE_PRIVATE);
+        programPreferences = context.getSharedPreferences(PROGRAM_PREFERENCES_NAME, Context.MODE_PRIVATE);
+        controllerPreferences = context.getSharedPreferences(CONTROLLER_SETUP_PREFERENCES, Context.MODE_PRIVATE);
     }
 
     public void setActiveBedroomProgramId(int programId) {
-        sharedPreferences.edit().putInt(ACTIVE_BEDROOM_PROGRAM, programId).apply();
+        programPreferences.edit().putInt(ACTIVE_BEDROOM_PROGRAM, programId).apply();
     }
 
     public int getActiveBedroomProgramId() {
-        return sharedPreferences.getInt(ACTIVE_BEDROOM_PROGRAM, 0);
+        return programPreferences.getInt(ACTIVE_BEDROOM_PROGRAM, 0);
+    }
+
+    public void setActiveLivingroomProgramId(int programId) {
+        programPreferences.edit().putInt(ACTIVE_LIVINGROOM_PROGRAM, programId).apply();
+    }
+
+    public int getActiveLivingroomProgramId() {
+        return programPreferences.getInt(ACTIVE_LIVINGROOM_PROGRAM, 0);
+    }
+
+    public void setBedroomControllerIpAddress(String controllerIpAddress) {
+        controllerPreferences.edit().putString(BEDROOM_IP_ADDRESS, controllerIpAddress).apply();
+    }
+
+    public String getBedroomControllerIpAddress() {
+        return controllerPreferences.getString(BEDROOM_IP_ADDRESS, null);
+    }
+
+    public void setBedroomControllerPort(int port) {
+        controllerPreferences.edit().putInt(BEDROOM_PORT, port).apply();
+    }
+
+    public int getBedroomControllerPort() {
+        return controllerPreferences.getInt(BEDROOM_PORT, -1);
+    }
+
+    public void setLivingroomControllerIpAddress(String controllerIpAddress) {
+        controllerPreferences.edit().putString(LIVINGROOM_IP_ADDRESS, controllerIpAddress).apply();
+    }
+
+    public String getLivingroomControllerIpAddress() {
+        return controllerPreferences.getString(LIVINGROOM_IP_ADDRESS, null);
+    }
+
+    public void setLivingroomControllerPort(int port) {
+        controllerPreferences.edit().putInt(LIVINGROOM_PORT, port).apply();
+    }
+
+    public int getLivingroomControllerPort() {
+        return controllerPreferences.getInt(LIVINGROOM_PORT, -1);
+    }
+
+    public void setLivingroomLedCount(int ledCount) {
+        controllerPreferences.edit().putInt(LIVINGROOM_LED_COUNT, ledCount).apply();
+    }
+
+    public int getLivingroomLedCount() {
+        return controllerPreferences.getInt(LIVINGROOM_LED_COUNT, -1);
     }
 
 }
--- androidClient/HomeMoodLigtingClient/app/src/main/java/com/nyekilajos/homemoodlightingclient/ui/settings/SettingsFragment.java
+++ androidClient/HomeMoodLigtingClient/app/src/main/java/com/nyekilajos/homemoodlightingclient/ui/settings/SettingsFragment.java
@@ -1,9 +1,100 @@
 package com.nyekilajos.homemoodlightingclient.ui.settings;
 
 import roboguice.fragment.RoboFragment;
+import roboguice.inject.InjectView;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.NumberPicker;
+import android.widget.TextView;
+
+import com.google.inject.Inject;
+import com.nyekilajos.homemoodlightingclient.R;
+import com.nyekilajos.homemoodlightingclient.persitency.LightingPreferencesHelper;
 
 /**
- * Created by Lajos_Nyeki on 11/30/2015.
+ * Fragment for settings about lighting controllers.
+ * 
+ * @author Lajos_Nyeki
  */
 public class SettingsFragment extends RoboFragment {
+
+    private static final int MIN_LED_NUM = 1;
+    private static final int MAX_LED_NUM = 1500;
+    private static final int DEFAULT_LED_NUM = 300;
+
+    @InjectView(R.id.bedroom_controller_ip)
+    private TextView bedroomControllerIp;
+    @InjectView(R.id.bedroom_controller_port)
+    private TextView bedroomControllerPort;
+    @InjectView(R.id.livingroom_controller_ip)
+    private TextView livingroomControllerIp;
+    @InjectView(R.id.livingroom_controller_port)
+    private TextView livingroomControllerPort;
+    @InjectView(R.id.led_num_picker)
+    private NumberPicker ledNumPicker;
+
+    @Inject
+    private LightingPreferencesHelper lightingPreferencesHelper;
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_settings, container, false);
+    }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        ledNumPicker.setMinValue(MIN_LED_NUM);
+        ledNumPicker.setMaxValue(MAX_LED_NUM);
+        ledNumPicker.setValue(DEFAULT_LED_NUM);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        loadValuesFromPreferences();
+    }
+
+    private void loadValuesFromPreferences() {
+        bedroomControllerIp.setText(lightingPreferencesHelper.getBedroomControllerIpAddress());
+
+        int bedroomPort = lightingPreferencesHelper.getBedroomControllerPort();
+        bedroomControllerPort.setText(bedroomPort != -1 ? Integer.toString(bedroomPort) : null);
+
+        livingroomControllerIp.setText(lightingPreferencesHelper.getLivingroomControllerIpAddress());
+
+        int livingroomPort = lightingPreferencesHelper.getLivingroomControllerPort();
+        livingroomControllerPort.setText(livingroomPort != -1 ? Integer.toString(lightingPreferencesHelper.getLivingroomControllerPort()) : null);
+
+        int ledCount = lightingPreferencesHelper.getLivingroomLedCount();
+        ledNumPicker.setValue(ledCount != -1 ? lightingPreferencesHelper.getLivingroomLedCount() : DEFAULT_LED_NUM);
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        storeValuesToPreferences();
+    }
+
+    private void storeValuesToPreferences() {
+        if (bedroomControllerIp.getText().length() != 0) {
+            lightingPreferencesHelper.setBedroomControllerIpAddress(bedroomControllerIp.getText().toString());
+        }
+        if (bedroomControllerPort.length() != 0) {
+            lightingPreferencesHelper.setBedroomControllerPort(Integer.parseInt(bedroomControllerPort.getText().toString()));
+        }
+        if (livingroomControllerIp.length() != 0) {
+            lightingPreferencesHelper.setLivingroomControllerIpAddress(livingroomControllerIp.getText().toString());
+        }
+        if (livingroomControllerPort.length() != 0) {
+            lightingPreferencesHelper.setLivingroomControllerPort(Integer.parseInt(livingroomControllerPort.getText().toString()));
+        }
+
+        lightingPreferencesHelper.setLivingroomLedCount(ledNumPicker.getValue());
+    }
 }
